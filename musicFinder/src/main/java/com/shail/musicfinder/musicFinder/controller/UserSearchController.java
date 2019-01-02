e8937ecc4972ad29454cdc9ec87bc098c9751d4f
shailendra93 <acharyashailendra1@gmail.com>
2019-01-02 23:42:56 +0530

    added user search api

--- musicFinder/src/main/java/com/shail/musicfinder/musicFinder/controller/UserSearchController.java
+++ musicFinder/src/main/java/com/shail/musicfinder/musicFinder/controller/UserSearchController.java
@@ -1,5 +1,55 @@
 package com.shail.musicfinder.musicFinder.controller;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.Future;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import com.shail.musicfinder.musicFinder.exceptions.InvalidInputException;
+import com.shail.musicfinder.musicFinder.objects.Track;
+import com.shail.musicfinder.musicFinder.objects.User;
+import com.shail.musicfinder.musicFinder.services.ResponseGenerationService;
+import com.shail.musicfinder.musicFinder.services.SearchResponse;
+import com.shail.musicfinder.musicFinder.services.UserResponse;
+
+@RestController
 public class UserSearchController {
 
+	@Autowired
+	private ResponseGenerationService responseGenService;
+	@Autowired
+	private SearchResponse searchService;
+	
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+	public DeferredResult<ResponseEntity<?>> addUser(@RequestBody String search) throws IOException {
+		System.out.println("searching  music");
+		DeferredResult<ResponseEntity<?>> deferedResult = responseGenService.generateDefferedResponse();
+
+		if (search == null) {
+			responseGenService.sendFalseResponse(deferedResult, new InvalidInputException("Input user is Null"));
+		} else {
+
+			if (search == null || search.trim().equalsIgnoreCase("")) {
+				responseGenService.sendFalseResponse(deferedResult,
+						new InvalidInputException("search string  is Null"));
+
+			} else {
+				// save rule
+				Future<List<Track>> searchResult =  searchService.findTrackDetails("Tum Hi Ho Bandhu");
+				responseGenService.sendTrueResponse(deferedResult,  searchResult);
+			}
+		}
+
+		return deferedResult;
+	}
+	
+	
 }
--- musicFinder/src/main/java/com/shail/musicfinder/musicFinder/services/SearchResponse.java
+++ musicFinder/src/main/java/com/shail/musicfinder/musicFinder/services/SearchResponse.java
@@ -4,91 +4,75 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
-import javax.lang.model.element.Element;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.internal.filter.ValueNode.JsonNode;
 import com.shail.musicfinder.musicFinder.objects.Track;
-import com.shail.musicfinder.musicFinder.objects.Tracks;
-
-
-
 
 
 @Service
 public class SearchResponse {
 
+	private static final ExecutorService threadpool = Executors.newCachedThreadPool();
 	@Value("${lastfm.apikey}")
-	private String apiKey= "3bfebc9ac2e9950b11b93427506fb470";
+	private String apiKey;
 	
 	@Cacheable("usersearch")
-	public  List<Track> findTrackDetails(String search) throws IOException
+	public Future<List<Track>> findTrackDetails(String search) throws IOException
 	{
-		System.out.println("inside track " +apiKey +" "+search);
-		RestTemplate restTemplate  = new RestTemplate();
+		return threadpool.submit(new Callable<List<Track>>() {
 		
-		String lastfmTrack = "http://ws.audioscrobbler.com/2.0/?method=track.search&track="+search+"&api_key="+apiKey+"&format=json";
-	
-		ResponseEntity<String> response = restTemplate.getForEntity(lastfmTrack , String.class);
-		ObjectMapper mapper = new ObjectMapper();
-		com.fasterxml.jackson.databind.JsonNode root = mapper.readTree(response.getBody());
-		com.fasterxml.jackson.databind.JsonNode name = root.get("results").get("trackmatches").get("track");
-		
-		System.out.println(name);
-		List<Track> tracks = new ArrayList<>();
-	
-		Iterator<com.fasterxml.jackson.databind.JsonNode> namenode = name.elements();
-		while(namenode.hasNext())
-		{
-			Track trak = new Track();
-			com.fasterxml.jackson.databind.JsonNode track = namenode.next();
-			trak.setName(track.get("name").toString());
-			trak.setArtist(track.get("artist").toString());
-			trak.setUrl(track.get("url").toString());
-			trak.setStreamable(track.get("streamable").toString());
-			trak.setListeners(track.get("listeners").toString());
-			trak.setMbid(track.get("mbid").toString());
-			tracks.add(trak);
+			@Override
+			public List<Track> call() throws Exception {
+				System.out.println("inside track " +apiKey +" "+search);
+				RestTemplate restTemplate  = new RestTemplate();
+				
+				String lastfmTrack = "http://ws.audioscrobbler.com/2.0/?method=track.search&track="+search+"&api_key="+apiKey+"&format=json";
 			
-		}
-		
-	     
-		 return tracks;
-		
-		
-		
-	}
-	
-	
-	
-	public static void main(String[] args) throws IOException  {
-		// TODO Auto-generated method stub
-
-		
-		SearchResponse sr = new SearchResponse();
-		List<Track> tk = sr.findTrackDetails("tum na ho");
-		for(Track tr: tk)
-		{
-			System.out.println(tr);
-		}
-		 tk = sr.findTrackDetails("tum hi ho");
-		for(Track tr: tk)
-		{
-			System.out.println(tr);
-		}
-		 tk = sr.findTrackDetails("tum hi ho");
-			for(Track tr: tk)
-			{
-				System.out.println(tr);
+				ResponseEntity<String> response = restTemplate.getForEntity(lastfmTrack , String.class);
+				ObjectMapper mapper = new ObjectMapper();
+				com.fasterxml.jackson.databind.JsonNode root = mapper.readTree(response.getBody());
+				com.fasterxml.jackson.databind.JsonNode name = root.get("results").get("trackmatches").get("track");
+				
+				System.out.println(name);
+				List<Track> tracks = new ArrayList<>();
+			
+				Iterator<com.fasterxml.jackson.databind.JsonNode> namenode = name.elements();
+				while(namenode.hasNext())
+				{
+					Track trak = new Track();
+					com.fasterxml.jackson.databind.JsonNode track = namenode.next();
+					trak.setName(track.get("name").toString());
+					trak.setArtist(track.get("artist").toString());
+					trak.setUrl(track.get("url").toString());
+					trak.setStreamable(track.get("streamable").toString());
+					trak.setListeners(track.get("listeners").toString());
+					trak.setMbid(track.get("mbid").toString());
+					tracks.add(trak);
+					
+				}
+				
+				
+			     
+				 return tracks;
 			}
 		
+		
+		});
+		
 	}
+	
+	
+	
+	
+	
 
 }
